54d1f3438d0db1629e05cd61e6aeffea60d16b19
Ricardo <aymardmb@gmail.com>
2021-11-10 15:20:41 +0100

    'react-icons' package added

--- dj-events-frontend/pages/events/index.js
+++ dj-events-frontend/pages/events/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 export default function EventsPage() {
     return (
         <Layout>
-            <h1>Coming up Events</h1>
+            <h1>Coming up Soon</h1>
         </Layout>
     )
 }
